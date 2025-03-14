dc3bc39356d378171d80443cdc9fcce208205feb
Atila Ferreira de Bastos <atila.bastos@paxbr.com.br>
2025-03-14 11:11:47 -0300

    CURSO_REACT

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -7,11 +7,14 @@ import ShowUserName from './components/ShowUserName';
 import ConditionalRender from './components/ConditionalRender';
 import CarDetails from './components/CarDetails';
 import Fragments from './components/Fragments';
+import Container from './components/Container';
+import ExecuteFunction from './components/ExecuteFunction';
+import Message from './components/Message';
+import ChangeMessageState from './components/ChangeMessageState';
 
 function App() {
  // const name ="Joaquim";
   const[username] = useState ("Maria");
-
 
   const cars = [
     {id: 1, brand: "FERRARI", color: "AMARELA", newCar: true, km: 0 },
@@ -19,9 +22,19 @@ function App() {
     {id: 1, brand: "FORD", color: "AZUL", newCar: false, km: 144000 },
   ]
 
+  function showMessage(){
+    console.log("Evento do componente pai!");
+  }
+
+  const [message, setMessage] = useState("");
+
+  const handleMessage = (msg) => {
+    setMessage (msg);
+  };
+
   return (
     <div className="App">
-        <h1>SISTEMA </h1>
+        <h1> Testando Site </h1>
         {/* Imagem em public */}
         <div>
           <img src="baixados.jfif" alt="Paisagem" />
@@ -42,6 +55,7 @@ function App() {
          {/* loop em array de objetos */}
          {cars.map((car) => (
           <CarDetails
+           key={car.id}
             brand={car.brand}
             color={car.color}
             km={car.km}
@@ -50,7 +64,19 @@ function App() {
          ))}
          {/* Fragment */}
         <Fragments propFragments ='teste' />
+         {/* children */}
+         <Container myValue ="testing">
+            <p>E este é o conteúdo</p>
+         </Container>
+         <Container myValue ="testing 2">
+            <p>Testando o container</p>
+         </Container>
+         {/* Executar função */}
+         <ExecuteFunction myFunction={showMessage} />
+         {/* State lift */}
+         <Message msg={message}/>
 
+         <ChangeMessageState handleMessage={handleMessage}/>
     </div>
   );
 }
