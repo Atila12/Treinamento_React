99ab5761b5c1a9800f47e2d505a524a0fbf730ea
Atila Ferreira de Bastos <atila.bastos@victumservices.com.br>
2025-04-28 16:21:17 -0300

    7_REQ_HTTP_atualização01

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,9 +1,65 @@
 import './App.css';
 
+import { useState, useEffect } from  "react";
+
+// URL base da API
+const url = "http://localhost:3000/products"
+
 function App() {
+  // salvando os dados 
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState ("");
+
+  // resgantando dados.
+  useEffect(() => {
+    async function fetchData() {
+
+      const res = await fetch(url);
+      const data = await res.json();
+  
+      setProducts(data);
+  
+    }
+    fetchData();
+  }, []);
+
+  // adicionar produtos
+  const handleSubmit = async (e) => {
+
+  }
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
+        ))}
+      </ul>
+      <div className='add-product'>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input 
+            type='text' 
+            value={name} 
+            name="name" 
+            onChange={(e) => setName(e.target.value)}/>
+          </label>
+          <label>
+            Preço:
+            <input 
+            type='number' 
+            value={price} 
+            name="price" 
+            onChange={(e) => setPrice(e.target.value)}/>
+          </label>
+          <input type='submit' value="Criar" />         
+        </form>
+      </div>
     </div>
   );
 }
